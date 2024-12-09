bf3cbe9ac4ad390394a0d2bddb67a6a41faec28c
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-09 17:33:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,30 @@ def get_movie_details(movie_name):
     else:
         return "Error fetching movie data."
 
+
+def get_movie_rating(movie_name):
+    """Fetches movie ratings from OMDb API."""
+    api_key = "a3b61eaa"  # Your OMDb API key
+    base_url = "http://www.omdbapi.com/?"
+    complete_url = f"{base_url}apikey={api_key}&t={movie_name}"
+    response = requests.get(complete_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data['Response'] == 'True':
+            ratings = data['Ratings']
+            rating_str = ""
+            for rating in ratings:
+                source = rating['Source']
+                value = rating['Value']
+                rating_str += f"{source}: {value}\n"
+            return rating_str
+        else:
+            return "Movie not found."
+    else:
+        return "Error fetching movie data."
+
+
 # --- Command Handler ---
 
 @bot.message_handler(commands=['start'])
@@ -49,7 +73,8 @@ def send_welcome(message):
     markup.add(
         telebot.types.InlineKeyboardButton('Time', callback_data='time'),
         telebot.types.InlineKeyboardButton('Date', callback_data='date'),
-        telebot.types.InlineKeyboardButton('Movie Details', callback_data='movie_details')
+        telebot.types.InlineKeyboardButton('Movie Details', callback_data='movie_details'),
+        telebot.types.InlineKeyboardButton('Movie Ratings', callback_data='movie_ratings')
     )
     bot.reply_to(message, "Hello! I'm a helpful bot. Choose an option:", reply_markup=markup)
 
@@ -65,6 +90,9 @@ def callback_query(call):
     elif call.data == "movie_details":
         bot.answer_callback_query(call.id, text="Send me a movie title to get details")
         bot.register_next_step_handler(call.message, process_movie_request)
+    elif call.data == "movie_ratings":
+        bot.answer_callback_query(call.id, text="Send me a movie title to get ratings")
+        bot.register_next_step_handler(call.message, process_movie_rating_request)
 
 def process_movie_request(message):
     """Processes the movie title and sends movie details."""
@@ -72,8 +100,14 @@ def process_movie_request(message):
     movie_info = get_movie_details(movie_name)
     bot.send_message(message.chat.id, movie_info)
 
+def process_movie_rating_request(message):
+    """Processes the movie title and sends movie ratings."""
+    movie_name = message.text
+    movie_ratings = get_movie_rating(movie_name)
+    bot.send_message(message.chat.id, movie_ratings)
+
 # --- Start the Bot ---
 
 if __name__ == '__main__':
     bot.infinity_polling()
-    
+        
