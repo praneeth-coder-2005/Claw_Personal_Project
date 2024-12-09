0fe97d4326cbabaa581b3b3f758348f63bcda20e
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-09 15:26:21 +0530

    Update test_credentials.py

--- test_credentials.py
+++ test_credentials.py
@@ -1,9 +1,9 @@
 import json
 import logging
 import os
-import pickle
 
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from telegram import Update
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 # Replace with your actual blog ID
 blog_id = "737863940949257967"
 
-# If modifying these scopes, delete the file token.pickle.
+# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/blogger"]
 
 # Path to your client credentials JSON file
@@ -37,12 +37,8 @@ CREDENTIALS_FILE = "credentials.json"  # Replace with the actual file name
 def authenticate():
     """Authenticates with the Blogger API and returns the service."""
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -51,8 +47,8 @@ def authenticate():
             flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
     return build("blogger", "v3", credentials=creds)
 
@@ -111,4 +107,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+        
