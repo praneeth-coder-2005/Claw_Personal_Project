6f01e955bddddc3156ab50da42673a8b8c95a54e
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-09 17:13:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Replace 'YOUR_BOT_TOKEN' with the actual token from BotFather
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+BOT_TOKEN = '7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk'
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
