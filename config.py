8b7eaff7bc44c13a627845a9002127db0bc2d26f
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-10 08:38:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-# config.py
-BOT_TOKEN = "7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk"  # Replace with your actual bot token
-OMDB_API_KEY = "YOUR_OMDB_API_KEY"  # Replace with your actual OMDb API key 
+BOT_TOKEN = '7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk'  # Replace with your actual bot token
+OMDB_API_KEY = 'a3b61eaa'  # Replace with your actual OMDb API key
+
+API_ID = 22250562  # Replace with your Telegram API ID
+API_HASH = '07754d3bdc27193318ae5f6e6c8016af'  # Replace with your Telegram API hash
