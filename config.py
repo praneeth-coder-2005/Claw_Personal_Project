91d5ed79ca4c2616debe0c779ca4dde7eb55d0d1
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-10 11:02:28 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-BOT_TOKEN = '7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk'  # Replace with your actual bot token
-OMDB_API_KEY = 'a3b61eaa'  # Replace with your actual OMDb API key
-
-API_ID = 22250562  # Replace with your Telegram API ID
-API_HASH = '07754d3bdc27193318ae5f6e6c8016af'  # Replace with your Telegram API hash
