3a29dbd4a3c012c8b7be4771ba9ae46702bcd08a
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-10 07:58:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-BOT_TOKEN = "YOUR_BOT_TOKEN"  # Replace with your actual bot token
+BOT_TOKEN = "7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk"  # Replace with your actual bot token
 OMDB_API_KEY = "YOUR_OMDB_API_KEY"  # Replace with your actual OMDb API key 
