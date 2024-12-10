590673b0662c303b5501d497c3fcd00a85073c82
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-10 15:21:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import telebot
 # --- Configuration ---
 
 BOT_TOKEN = '7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk'  # Replace with your actual bot token
-TMDB_API_KEY = "b20fd82c1814084ba3f09890d94f9895"  # Replace with your actual TMDb API key
+TMDB_API_KEY = "bb5f40c5be4b24660cbdc20c2409835e"  # Replace with your actual TMDb API key
 
 # --- Logging ---
 
