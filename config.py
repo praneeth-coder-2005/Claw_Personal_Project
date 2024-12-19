1b0942555a2bbc73f2658f745432920867299a7c
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-19 07:47:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = "7805737766:AAEAOEQAHNLNqrT0D7BAeAN_x8a-RDVnnlk" # Replace with your actual bot token
+TMDB_API_KEY = "bb5f40c5be4b24660cbdc20c2409835e" # Replace with your actual TMDb API key
